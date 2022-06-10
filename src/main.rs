f0ec7984a692b345c57fbbfa156884584d80a50b
iogeo <99806509+iogeo@users.noreply.github.com>
2022-06-10 04:37:41 -0700

    Add files via upload

---
+++ src/main.rs
@@ -0,0 +1,52 @@
+use std::io::prelude::*;
+use axum::{Json, response::{Html, IntoResponse, Response},routing::get,Router,http::{Uri, header::{self, HeaderMap, HeaderName}},};
+use std::fs::File;
+use std::fs;
+use std::str::FromStr;
+use std::env;
+use axum::body::Full;
+
+struct js
+{
+    String:String
+}
+
+async fn response() -> axum::http::response::Builder {
+    Response::builder()
+}
+
+async fn root() -> Html<String>{
+    let mut r=File::open("index.html").unwrap();
+    let mut p = String::new();
+    r.read_to_string(&mut p);
+    Html(p)
+}
+
+async fn pkgjs() -> impl IntoResponse{
+    let mut r=File::open("ffmpeg.min.js").unwrap();
+    let mut p = String::new();
+    r.read_to_string(&mut p);
+    response()
+        .await.status(200)
+        .header("Content-Type","text/javascript; charset=UTF-8")
+        .header("Cross-Origin-Embedder-Policy","require-corp")
+        .header("Cross-Origin-Opener-Policy:same-origin","same-origin")
+        .body(Full::from(p))
+        .unwrap()
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route(
+        "/", get(root))
+        .route(
+        "/ffmpeg.min.js", get(pkgjs));
+    let q = env::var("PORT")
+        .unwrap()
+        .to_string();
+    axum::Server::bind(&("0.0.0.0:".to_owned()+&q).parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
